315e293f067b2c2bb2e5bd28952b2ad77f1ce69d
Bhumika <bhs672001@gmail.com>
2021-06-26 01:29:43 +0530

    Updated app.js

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000; //runs on local host 5000
 
 app.use(express.static("public"));
 
@@ -16,16 +16,21 @@ io.on("connection", (socket) => {
   socket.on("create or join", (room) => {
     console.log("create or join the room", room);
     room = room.trim();
-    const myRoom = io.sockets.adapter.rooms.get(room) || { size: 0 };
+    const myRoom = io.sockets.adapter.rooms.get(room) || { size: 0 }; //to get the size of the room
     const numClients = myRoom.size;
     console.log(room, "has", numClients, "clients");
 
+    //if room is empty and is to be created for the first time
     if (numClients === 0) {
       socket.join(room);
-      socket.emit("created", room);
+      socket.emit("created", room); //event "created" defined in main.js
+
+      //if room is already created
     } else if (numClients === 1) {
       socket.join(room);
-      socket.emit("joined", room);
+      socket.emit("joined", room); //event "joined" defined in main.js
+
+      //room full
     } else {
       socket.emit("full", room);
     }
@@ -40,10 +45,12 @@ io.on("connection", (socket) => {
   });
 
   socket.on("offer", (event) => {
+    //offer call
     socket.broadcast.to(event.room).emit("offer", event.sdp);
   });
 
   socket.on("answer", (event) => {
+    //answer call
     socket.broadcast.to(event.room).emit("answer", event.sdp);
   });
 });
