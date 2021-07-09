d67eaf4c168b7d56dbd567f352d7748bf1c4377b
Bhumika <bhs672001@gmail.com>
2021-07-09 15:23:57 +0530

    Update group call

--- src/app.js
+++ src/app.js
@@ -2,14 +2,12 @@ let express = require("express");
 let app = express();
 let server = require("http").Server(app);
 let io = require("socket.io")(server);
-let stream = require("./ws/stream");
+let stream = require("./public/stream");
 let path = require("path");
-//let favicon = require( 'serve-favicon' );
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
-//app.use( favicon( path.join( __dirname, 'favicon.ico' ) ) );
-app.use("/assets", express.static(path.join(__dirname, "assets")));
+app.use("/public", express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
