4ebe4679a8a35b3bb4aad19de339e39b0018de9d
Bhumika <bhs672001@gmail.com>
2021-07-10 13:29:33 +0530

    Infinite screens in screen share :/

--- src/public/js/rtc.js
+++ src/public/js/rtc.js
@@ -297,5 +297,78 @@ window.addEventListener("load", () => {
 
       broadcastNewTracks(myStream, "video");
     });
+
+    document.getElementById("share-screen").addEventListener("click", (e) => {
+      e.preventDefault();
+
+      if (
+        screen &&
+        screen.getVideoTracks().length &&
+        screen.getVideoTracks()[0].readyState != "ended"
+      ) {
+        stopSharingScreen();
+      } else {
+        shareScreen();
+      }
+    });
+
+    function shareScreen() {
+      h.shareScreen()
+        .then((stream) => {
+          h.toggleShareIcons(true);
+
+          //disable the video toggle btns while sharing screen. This is to ensure clicking on the btn does not interfere with the screen sharing
+          //It will be enabled was user stopped sharing screen
+          h.toggleVideoBtnDisabled(true);
+
+          //save my screen stream
+          screen = stream;
+
+          //share the new stream with all partners
+          broadcastNewTracks(stream, "video", false);
+
+          //When the stop sharing button shown by the browser is clicked
+          screen.getVideoTracks()[0].addEventListener("ended", () => {
+            stopSharingScreen();
+          });
+        })
+        .catch((e) => {
+          console.error(e);
+        });
+    }
+
+    function stopSharingScreen() {
+      //enable video toggle btn
+      h.toggleVideoBtnDisabled(false);
+
+      return new Promise((res, rej) => {
+        screen.getTracks().length
+          ? screen.getTracks().forEach((track) => track.stop())
+          : "";
+
+        res();
+      })
+        .then(() => {
+          h.toggleShareIcons(false);
+          broadcastNewTracks(myStream, "video");
+        })
+        .catch((e) => {
+          console.error(e);
+        });
+    }
+
+    document.getElementById("share-screen").addEventListener("click", (e) => {
+      e.preventDefault();
+
+      if (
+        screen &&
+        screen.getVideoTracks().length &&
+        screen.getVideoTracks()[0].readyState != "ended"
+      ) {
+        stopSharingScreen();
+      } else {
+        shareScreen();
+      }
+    });
   }
 });
